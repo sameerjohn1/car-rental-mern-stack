047013d639e408c1110812db59660a39e92d7d0a
sameerjohn1 <sameerprogrammer5@gmail.com>
2026-03-24 07:11:08 +0500

    Added Hero Banner (Home Page) with interactive animation

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import HomeBanner from "../components/HomeBanner";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <HomeBanner />
     </div>
   );
 };
