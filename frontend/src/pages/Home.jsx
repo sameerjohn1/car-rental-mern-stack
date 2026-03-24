187b089348ccaae3f79db30faec97d2bfb8503b1
sameerjohn1 <sameerprogrammer5@gmail.com>
2026-03-24 14:03:17 +0500

    Added Testimonials component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import HomeBanner from "../components/HomeBanner";
 import HomeCars from "../components/HomeCars";
+import Testimonials from "../components/Testimonials";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Navbar />
       <HomeBanner />
       <HomeCars />
+      <Testimonials />
     </div>
   );
 };
