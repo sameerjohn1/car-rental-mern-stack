43a1e9fa961169d5e314855296e92e2e87f29689
sameerjohn1 <sameerprogrammer5@gmail.com>
2026-03-24 07:51:09 +0500

    Added HomeCars component with animated car cards and booking navigation

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import HomeBanner from "../components/HomeBanner";
+import HomeCars from "../components/HomeCars";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <HomeBanner />
+      <HomeCars />
     </div>
   );
 };
