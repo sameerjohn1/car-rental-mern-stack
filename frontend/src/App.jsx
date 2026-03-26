34c9ffd218453dec4e6870a5bc5b996efb18c9b9
sameerjohn1 <sameerprogrammer5@gmail.com>
2026-03-26 14:59:27 +0500

    feat: add contact page with responsive UI and WhatsApp integration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
+import Contact from "./pages/Contact";
 
 const App = () => {
   return (
@@ -10,6 +11,8 @@ const App = () => {
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<SignUp />} />
+
+      <Route path="/contact" element={<Contact />} />
     </Routes>
   );
 };
